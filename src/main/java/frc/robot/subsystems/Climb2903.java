371b64bc1b9082c33f63847081f891ce4ef71123
Robot Club <robot@hsstudent.arlington.asd>
2020-01-13 16:37:09 -0800

    Base climb clas. Literally does nothing.

---
+++ src/main/java/frc/robot/subsystems/Climb2903.java
@@ -0,0 +1,20 @@
+//Climber - Top of robot, on both sides. Starts within robot frame, can extend up and then further outwards, clips to bar, and pulls down to lift robot.
+//Hardware involved:
+//- Two pneumatic solenoids: for raising and lowering climb arms (MUST NOT lower arms if robot is climbing)
+//- Two larger pneumatic solenoids: for reaching towards bar and pulling robot up. (MUST NOT extend if smaller solenoids are closed)
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Climb2903 extends SubsystemBase {
+
+    public Climb2903() {
+
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+}
